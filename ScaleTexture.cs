431f04afbcb629604d159b802919be9aa5704e72
jmjohnson5412 <jmjohnson5412@msn.com>
2018-05-31 21:03:50 -0500

    First changes
    Player and network folder

---
+++ ScaleTexture.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTexture : MonoBehaviour {
+
+    private float repeatEvery = 40f;
+
+	void Start () {
+         gameObject.GetComponent<Renderer>().material.mainTextureScale = new Vector2(transform.localScale.x, transform.localScale.y);
+    }
+}
